48ced81bccee30e49318b4379c51c5d060ea4ddc
tzintzenet <mmicroshit@outlook.com>
2023-11-15 13:37:49 +0200

    fixed bug: crashing when trying to save a file

--- SaveLogs.py
+++ SaveLogs.py
@@ -1,9 +1,16 @@
 import datetime
 import EditConfig
+import os
 
 def write(data, name=None):
 
+    savePath = EditConfig.getData().get('save-location') + '/'
+
     if name is None:
         name = 'log_from_' + str(datetime.datetime.now())
-    logFile = open(EditConfig.getData().get('save-location') + '/' + name, 'w')
+
+    if not os.path.exists(savePath):
+        os.makedirs(savePath)
+
+    logFile = open(savePath + name, 'w')
     logFile.write(data)
