dfbc0113eccb44c206e7c8180badcfc49a6f86b2
tzintzenet <mmicroshit@outlook.com>
2023-11-27 15:43:05 +0200

    fixed crash when specifically Windows sucks

--- SaveLogs.py
+++ SaveLogs.py
@@ -7,11 +7,23 @@ def write(data, name=None):
 
     savePath = EditConfig.getData().get('save-location') + '/'
 
+
     if name is None:
         name = 'log_from_' + str(datetime.datetime.now())
 
     if not os.path.exists(savePath):
         os.makedirs(savePath)
 
-    logFile = open(savePath + name, 'w')
+    file_number = 0
+    if os.path.isfile(name):
+        while not os.path.isfile(name + str(file_number)):
+            file_number += 1
+
+    file_number = str(file_number)
+
+    if file_number == "0":
+        logFile = open(savePath + name, 'x')
+    else:
+        logFile = open(savePath + name + file_number, 'x')
+
     logFile.write(data)
